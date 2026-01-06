4d0ea902abd85c3ce47b2c2a379029ab55879ae4
Nelson Grac-Aubert <nelson.grac-aubert@laplateforme.io>
2026-01-06 09:48:21 +0100

    Added 24/ampm mode to clock function, added commented out prints for testing

--- clock.py
+++ clock.py
@@ -22,17 +22,20 @@ def set_alarm(t):
     return alarm
 
 def display_setting(mode = 24): 
+    """ mode est 24 par défaut et correspond a l'affichage 23:59
+    si mode = 12, l'affichage sera AM/PM """
     if mode == 24 : 
         print("24:00 display mode set")
     elif mode == 12 : 
         print("AM/PM display mode set")
+    return mode 
 
-def clock(current_time, alarm):
+def clock(current_time, alarm, mode):
     """ current_time est le tuple retourné par afficher_heure() 
     alarm est le tuple retourné par set_alarm 
     actualise et imprime l'heure toutes les secondes et affiche un message pour l'alarme """
 
-    while True:
+    while mode == 24 :
         print(f"{current_time[0]:02d}:{current_time[1]:02d}:{current_time[2]:02d}")
         if current_time == alarm:
             print("Wake up granny Jeannine!")
@@ -49,9 +52,17 @@ def clock(current_time, alarm):
             h = 0
         current_time = (h, m, s)
 
+    if mode == 12 :
+        if alarm[0] >= 12 : 
+            alarm_am_pm_format = (alarm[0]-12, alarm[1], alarm[2], "PM")
+        else : 
+            alarm_am_pm_format = (alarm[0], alarm[1], alarm[2], "AM")
+        # print (alarm_am_pm_format)
+  
 if __name__ == "__main__" : 
 
     current_time = afficher_heure((12, 0, 0))
     alarm = set_alarm((12, 0, 10))
-    display_setting()
-    clock(current_time, alarm)
+    # mode = display_setting(12)
+    mode = display_setting(24)
+    clock(current_time, alarm, mode)
