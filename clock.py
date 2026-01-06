22db73acb0733faf0cbbcc322d79e73665417db2
Nelson Grac-Aubert <nelson.grac-aubert@laplateforme.io>
2026-01-06 09:18:53 +0100

    Removed global variables, put them as arguments and returns

--- clock.py
+++ clock.py
@@ -1,25 +1,24 @@
 import time
 
 def afficher_heure(t=None):
-    global current_time
     if t is None:
         local = time.localtime()
         current_time = (local.tm_hour, local.tm_min, local.tm_sec)
     else:
         current_time = t
+    return current_time
 
 def set_alarm(t):
-    global alarm
     alarm = t
     print(f"Alarm set at {alarm[0]:02d}:{alarm[1]:02d}:{alarm[2]:02d}")
+    return alarm
 
-def clock():
-    global current_time, alarm
+def clock(current_time, alarm):
     while True:
         # :02d = on remplit avec des 0 si nécessaire, pour que cela fasse 2 caractères, d comme decimal integer
         print(f"{current_time[0]:02d}:{current_time[1]:02d}:{current_time[2]:02d}")
         if current_time == alarm:
-            print("Réveille toi Mamie!")
+            print("Wake up granny Jeannine!")
         time.sleep(1)
         h, m, s = current_time
         s += 1
@@ -35,6 +34,6 @@ def clock():
 
 if __name__ == "__main__" : 
 
-    afficher_heure((12, 0, 0))
-    set_alarm((12, 0, 10))
-    clock()
+    current_time = afficher_heure((12, 0, 0))
+    alarm = set_alarm((12, 0, 10))
+    clock(current_time, alarm)
